8da8a91306fc08d71100d93b8bf69dab6ef20db2
nasfaw <nardosasfaw77@gmail.com>
2024-10-26 15:49:48 -0400

    task 5 edited

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,5 @@
 USE alx_book_store;
 
--- Create the customer table if it doesn't exist
 CREATE TABLE IF NOT EXISTS customer (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(100),
@@ -8,8 +7,8 @@ CREATE TABLE IF NOT EXISTS customer (
     address VARCHAR(100)
 );
 
--- Insert data into the customer table
-INSERT IGNORE INTO customer (customer_id, customer_name, email, address)
+
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
